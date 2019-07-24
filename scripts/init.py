fe18008e38138c0861f815dcf5ffe65502e286bb
JRoemer <jroemer@agh.edu.pl>
2019-07-24 18:39:07 +0200

    1. Add new file: requirement.txt

--- scripts/init.py
+++ scripts/init.py
@@ -1,4 +1,11 @@
+teams = list()
+teams.append("Wisla")
+teams.append("Cracowia")
+teams.append("Lech")
+teams.append("Legia")
+teams.append("Hutnik")
+teams.append("Widzew")
 
+print([team for team in teams])
 
-A = list()
-
+install openpyxl
