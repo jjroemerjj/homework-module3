f305c3f9cd8cbf4ce54bce6a634ea9dda83f2eae
jjroemerjj <52920190+jjroemerjj@users.noreply.github.com>
2019-07-24 13:35:21 +0200

    Feature 2 team (#3)
    * 1. Player class - add arguments and basic methods
    
    * 1. Team class - add arguments and basic methods

--- scripts/team.py
+++ scripts/team.py
@@ -1,4 +1,21 @@
 
 class Team:
-    def __init__(self):
-        pass
+    def __init__(self, name: str):
+        self.name: str = name
+        self.score: int = 0
+        self.players: list = []
+        self.budget: int = 1000     #this value probably has to be changed (asses Player caclulate.value() method)
+
+    def description(self):
+        return f"We are {self.name}, our score is {self.score} and we have budget of {self.budget} PLN"
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def add_score(self, score):
+        self.score += score
+
+    def update_budget(self, value):
+        self.budget += value
+
+
